522e24c6826f59569984959392a2561e72e012d6
chrisbermejo <chrisbermejo1@gmail.com>
2023-08-22 00:46:46 -0400

    input bar, planning on adding better input bar

--- client/src/components/Chatroom/InputBar.jsx
+++ client/src/components/Chatroom/InputBar.jsx
@@ -5,9 +5,9 @@ export function ChatRoomInputBar() {
     const { sendMessage, message, setMessage } = useInfo();
 
     const handleKeyPress = (e) => {
-        if (e.key === 'Enter' && e.shiftKey) {
-            e.target.rows = e.target.rows + 1;
-        }
+        // if (e.key === 'Enter' && e.shiftKey) {
+        //     e.target.rows = e.target.rows + 1;
+        // }
         if (e.key === 'Enter' && !e.shiftKey) {
             e.preventDefault();
             sendMessage();
@@ -17,8 +17,16 @@ export function ChatRoomInputBar() {
 
     return (
         <div className='chatroom-inputs-container'>
-            <textarea
+            {/* <textarea
                 rows={1}
+                className='chatroom-input'
+                type="text"
+                placeholder="Message..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={handleKeyPress}
+            /> */}
+            <input
                 className='chatroom-input'
                 type="text"
                 placeholder="Message..."
